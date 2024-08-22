7fccaf00bcaae6f85f46b77fddb4f834a980fc76
Sha-x2-nk <shashankrajora2002@gmail.com>
2024-08-22 13:42:10 +0530

    dwm, slstatus

--- scripts/drivers/audio.sh
+++ scripts/drivers/audio.sh
@@ -4,6 +4,4 @@ audioSetup() {
     # Audio support in Linux
     sudo nala install pipewire -y
     systemctl --user --now enable pipewire pipewire-pulse
-    # For managing audio output/input devices
-    sudo nala install pavucontrol -y
 }
