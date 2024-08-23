5f3c20596661a65f8e6241e4ae823d09f1eed7fb
Shashank Rajora <97790393+Sha-x2-nk@users.noreply.github.com>
2024-08-23 07:49:24 +0530

    Update wifi_bt.sh

--- scripts/drivers/wifi_bt.sh
+++ scripts/drivers/wifi_bt.sh
@@ -2,6 +2,7 @@
 
 WifiBtSetup() {
     # Wifi & Bluetooth for Intel AX series interface
+    sudo apt --purge autoremove bluez -y
     sudo nala install firmware-iwlwifi -y
     sudo modprobe -r iwlwifi ; sudo modprobe iwlwifi
 }
